435f2ab10d95c8a31c17f5cca203c3a9059bc051
mamba1202 <1612786059@qq.com>
2020-11-22 19:13:47 +0800

    build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+console.log(logo)
 
+function sayHi() {
+  alert('这是react的点击事件')
+}
 function App() {
   return (
+    //XML
+    //会将下面标签变成js -JSX
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React也不过如此
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>编辑App.js就能改变页面展示</p>
+        <button onClick={ function(){alert('页面内的点击事件')} }>点击事件</button>
       </header>
-    </div>
+    </div >
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ ReactDOM.render(
   </React.StrictMode>,
   div
 );
+// 实际上是在js 里面写xml
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

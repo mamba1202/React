8a3e20bfc90a3574b33d2079fe0cd211333434b4
mamba1202 <1612786059@qq.com>
2020-11-29 23:10:24 +0800

    react父子通信class

--- src/components/PassByValue.js
+++ src/components/PassByValue.js
@@ -3,36 +3,63 @@
 import '../components/PassByValue.css'
 import React, { Component } from 'react'
 
-function AppPass() {
-    return (
-        <div>
 
-            <div className="header">
-                <Time1></Time1>
-                <Judge></Judge>
-                <Time2></Time2>
+class AppPass extends Component {
+    constructor() {
+        super()
+        this.state = {
+            result1: 0,
+            result2: 0,
+        }
+        // this.t0 = new Date()
+    }
+
+    success1() {
+        console.log('兔子跑完了')
+        let t1 = new Date - 0
+        this.setState({
+            result1: t1
+        })
+    }
+    success2() {
+        console.log('乌龟跑完了')
+        let t1 = new Date - 0
+        this.setState({
+            result2: t1
+        })
+    }
+
+    render() {
+        return (
+            <div>
+
+                <div className="header">
+                    <Time1 result={this.state.result1}></Time1>
+                    <Judge></Judge>
+                    <Time2 result={this.state.result2}></Time2>
+                </div>
+                <Track1 success1={this.success1.bind(this)}></Track1>
+                <Track2 success2={this.success2.bind(this)}></Track2>
             </div>
-            <Track1></Track1>
-            <Track2></Track2>
-        </div>
 
-    )
+        )
+    }
 }
 
-function Time1() {
+function Time1(props) {
     return (
         <div>
             <h2>🐇用时</h2>
-            <div>0</div>
+            <div>{props.result}</div>
         </div>
     )
 }
 
-function Time2() {
+function Time2(props) {
     return (
         <div>
             <h2>🐢用时</h2>
-            <div>0</div>
+            <div>{props.result}</div>
         </div>
     )
 }
@@ -43,8 +70,8 @@ function Judge() {
 }
 
 class Track1 extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         let n = 0
         this.state = {
             style: {
@@ -53,7 +80,7 @@ class Track1 extends Component {
         }
 
         let playerId = setInterval(() => {
-           
+
             n += 10
 
             this.setState({
@@ -63,11 +90,13 @@ class Track1 extends Component {
             })
             if (n >= 50) {
                 window.clearInterval(playerId)
+                console.log(this.props)
+                this.props.success1()
             }
-    
+
         }, 1000)
 
-      
+
     }
 
     render() {
@@ -81,8 +110,8 @@ class Track1 extends Component {
 
 }
 class Track2 extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         let n = 0
         this.state = {
             style: {
@@ -91,7 +120,7 @@ class Track2 extends Component {
         }
 
         let playerId = setInterval(() => {
-           
+
             n += 2
 
             this.setState({
@@ -99,13 +128,15 @@ class Track2 extends Component {
                     transform: `translate(${n}%)`
                 }
             })
-            if (n >50) {
+            if (n > 50) {
                 window.clearInterval(playerId)
+                this.props.success2()
+
             }
-    
+
         }, 1000)
 
-     
+
     }
 
     render() {
